f8437dc23a78aac27e411c8a58cb399f43405491
Feitosa Junior <juniorfeito2016@gmail.com>
2023-11-26 10:35:55 -0300

    Add P&R 10 e 14

---
+++ Pensar e responder 10/index.js
@@ -0,0 +1,27 @@
+// Retorna o cosseno de 180 graus
+
+var cos = Math.cos((180 * Math.PI) / 180);
+
+// Retorna o valor de 9 elevado à potência de 3
+
+var potencia = Math.pow(9, 3);
+
+// Retorna a raiz quadrada de 49
+
+var raiz = Math.sqrt(49)
+
+// Retorna o valor máximo entre 0 e 10
+
+var maximo = Math.max(0, 10)
+
+// Retorna um número aleatório entre 0 e 10
+
+var aleatorio = parseInt(Math.random() * 11)
+
+
+console.log("Atiividade Prática aula ")
+console.log("O cosseno de 180° é: " + cos);
+console.log("O número 9 elevado à potência de 3 é: " + potencia);
+console.log("A raiz quadrade de 49 é:" + raiz);
+console.log("O máximo entre 0 e 10: " + maximo);
+console.log("Um número aleatório entre 0 e 10: " + aleatorio);
